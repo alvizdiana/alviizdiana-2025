d6fed4846a91cd0560791b6d7bf70a1d2e4598f0
Alvi Izdiana <alfiizdiana@gmail.com>
2025-11-10 12:15:10 +0700

    Fix Vitbase path

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite';
 
+const repoName = '/alviizdiana-2025/';
+
 // https://vite.dev/config/
 export default defineConfig({
+  base: repoName,
   plugins: [react(), tailwindcss()],
 })
