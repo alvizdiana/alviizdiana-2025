67cf05eb3ebc198d82341d42e00f2f7728c52688
Alvi Izdiana <alfiizdiana@gmail.com>
2025-11-10 12:02:38 +0700

    Updates

---
+++ assets/asset.js
@@ -0,0 +1,71 @@
+import logo_dark from './logo_dark.svg'
+import logo_light from './logo_light.svg'
+import moon from './moon.svg'
+import sun from './sun.svg'
+import arrow from './arrow.svg'
+import alvi from './alvi.png'
+import email from './email.svg'
+import github from './github.svg'
+import linkedin from './linkedin.svg'
+import whatsapp from './whatsapp.svg'
+import email_navy from './email_navy.svg'
+import github_navy from './github_navy.svg'
+import linkedin_navy from './linkedin_navy.svg'
+import whatsapp_navy from './whatsapp_navy.svg'
+import gmail_navy from './gmail_navy.svg'
+import arrow_up_right from './arrow_up_right.svg'
+import download from './download.svg'
+import Depressive_es from './Depressive_es.png'
+import interactive_landing_page from './interactive_landing_page.png'
+import plotly_dash from './plotly_dash.png'
+import tableau_dash from './tableau_dash.png'
+import vue_portfolio from './vue_portfolio.png'
+import codeigniter_logo from './codeigniter_logo.svg'
+import css_logo from './css_logo.svg'
+import drawio_logo from './drawio_logo.svg'
+import figma_logo from './figma_logo.svg'
+import html_logo from './html_logo.svg'
+import javascript_logo from './javascript_logo.svg'
+import jupyter_logo from './jupyter_logo.svg'
+import mysql_logo from './mysql_logo.svg'
+import notion_logo from './notion_logo.svg'
+import python_logo from './python_logo.svg'
+import react_logo from './react_logo.svg'
+import vue_logo from './vue_logo.svg'
+
+export default {
+  logo_dark,
+  logo_light,
+  moon,
+  alvi,
+  sun,
+  arrow,
+  email,
+  github,
+  linkedin,
+  whatsapp,
+  email_navy,
+  github_navy,
+  linkedin_navy,
+  whatsapp_navy,
+  gmail_navy,
+  arrow_up_right,
+  download,
+  Depressive_es,
+  interactive_landing_page,
+  plotly_dash,
+  tableau_dash,
+  vue_portfolio,
+  codeigniter_logo,
+  css_logo,
+  drawio_logo,
+  figma_logo,
+  html_logo,
+  javascript_logo,
+  jupyter_logo,
+  mysql_logo,
+  notion_logo,
+  python_logo,
+  react_logo,
+  vue_logo
+}
